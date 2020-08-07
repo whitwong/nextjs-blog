79fdd06f3826081cb3feeb9952e174e26fc3bba7
Whitney Wong <whitney.wong@utexas.edu>
2020-08-07 10:37:33 -0500

    Polishing Post Page
    Added date-fns package to project for date formatting. Added new date.js component to take in date string as a prop and format it 'FullMonthName, #, ####'. Imported Date component to [id].js. Added utils.module.css stying to [id].js.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,16 +1,20 @@
 import Layout from '../../components/layout';
+import Head from 'next/head';
 import { getAllPostIds, getPostData } from '../../lib/posts';
+import utilStyles from '../../styles/utils.module.css';
+import Date from '../../components/date';
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}/>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}><Date dateString={postData.date} /></div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}/>
+      </article>
     </Layout>
   )
 }
